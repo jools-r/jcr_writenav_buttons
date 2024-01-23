3fe13819a850ba3d33d987492405f405c1ff60fb
jools-r <julian@txpbuilders.com>
2024-01-23 09:43:24 +0100

    wrap in a class: prevent possible future clashes

--- jcr_write_nextprev.php
+++ jcr_write_nextprev.php
@@ -6,44 +6,56 @@
  */
 
 if (txpinterface === 'admin') {
-    if ($event == 'article') {
-        register_callback(function($event, $step, $pre, $rs) {
-            return article_partial_article_nextprev($rs);
-        }, 'article_ui', 'extend_col_1', 0);
+    if ($event === 'article') {
+        new jcr_write_nextprev();
     }
 }
 
-/**
- * Renders next/prev links.
- *
- * @param      array $rs Article data
- * @return     string HTML
- *
- * wholesale duplicate of built-in article_partial_article_nav($rs)
- * should it someday be removed from the core (status: deprecated in 4.9.0)
- */
-
-function article_partial_article_nextprev($rs)
+class jcr_write_nextprev
 {
-    $out = array();
-
-    if ($rs['prev_id']) {
-        $out[] = prevnext_link(gTxt('prev'), 'article', 'edit', $rs['prev_id'], '', 'prev');
-    } else {
-        $out[] = span(gTxt('prev'), array(
-            'class'         => 'navlink-disabled',
-            'aria-disabled' => 'true',
-        ));
+    /**
+     * Initialise.
+     */
+    public function __construct()
+    {
+        // Hook into the Write panel callback at end of sidebar
+        register_callback(function($event, $step, $pre, $rs) {
+            return $this->article_partial_article_nextprev($rs);
+        }, 'article_ui', 'extend_col_1', 0);
     }
 
-    if ($rs['next_id']) {
-        $out[] = prevnext_link(gTxt('next'), 'article', 'edit', $rs['next_id'], '', 'next');
-    } else {
-        $out[] = span(gTxt('next'), array(
-            'class'         => 'navlink-disabled',
-            'aria-disabled' => 'true',
-        ));
-    }
+    /**
+     * Renders next/prev links.
+     *
+     * @param      array $rs Article data
+     * @return     string HTML
+     *
+     * wholesale duplicate of built-in article_partial_article_nav($rs)
+     * should it someday be removed from the core (status: deprecated in 4.9.0)
+     */
 
-    return n.tag(join('', $out), 'nav', array('class' => 'nav-tertiary'));
+    protected function article_partial_article_nextprev($rs)
+    {
+        $out = array();
+
+        if ($rs['prev_id']) {
+            $out[] = prevnext_link(gTxt('prev'), 'article', 'edit', $rs['prev_id'], '', 'prev');
+        } else {
+            $out[] = span(gTxt('prev'), array(
+                'class'         => 'navlink-disabled',
+                'aria-disabled' => 'true',
+            ));
+        }
+
+        if ($rs['next_id']) {
+            $out[] = prevnext_link(gTxt('next'), 'article', 'edit', $rs['next_id'], '', 'next');
+        } else {
+            $out[] = span(gTxt('next'), array(
+                'class'         => 'navlink-disabled',
+                'aria-disabled' => 'true',
+            ));
+        }
+
+        return n.tag(join('', $out), 'nav', array('class' => 'nav-tertiary'));
+    }
 }
